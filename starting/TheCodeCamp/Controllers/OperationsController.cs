fad5eb601521cad6322838c0e560602f79f55fa8
amyyan210 <amy@yanaway.com>
2019-07-23 11:24:44 -0500

    Create functional API to refresh app settings

---
+++ starting/TheCodeCamp/Controllers/OperationsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class OperationsController : ApiController
+    {
+        [Route("api/refreshconfig")]
+        [HttpOptions]
+        public IHttpActionResult RefreshAppSettings()
+        {
+            try
+            {
+                ConfigurationManager.RefreshSection("AppSettings");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
