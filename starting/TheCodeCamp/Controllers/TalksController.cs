df5cae5487cc69b0effdcaa5641c23796114dea8
amyyan210 <amy@yanaway.com>
2019-07-23 10:16:20 -0500

    Create an association controller for talks: * Implement getting an individual talk * Add mapping for talks and speakers that fixed issues with query string not working

---
+++ starting/TheCodeCamp/Controllers/TalksController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TheCodeCamp.Data;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Controllers
+{
+    [RoutePrefix("api/camps/{moniker}/talks")]
+    public class TalksController : ApiController
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+        public TalksController(ICampRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [Route()]
+        public async Task<IHttpActionResult> Get(string moniker, bool includeSpeakers = false)
+        {
+            try
+            {
+                var results = await _repository.GetTalksByMonikerAsync(moniker, includeSpeakers);
+
+                // Mapping
+                var mappedResult = _mapper.Map<IEnumerable<TalkModel>>(results);
+
+                return Ok(mappedResult);
+            }
+            catch (Exception ex)
+            {
+                // TODO: Add logging
+                return InternalServerError(ex);
+            }
+        }
+
+        [Route("{id:int}")]
+        public async Task<IHttpActionResult> Get(string moniker, int id, bool includeSpeakers = false)
+        {
+            try
+            {
+                var result = await _repository.GetTalkByMonikerAsync(moniker, id, includeSpeakers);
+
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                var mappedResult = _mapper.Map<TalkModel>(result);
+
+                return Ok(mappedResult);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
